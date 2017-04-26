07d417979c3dbd24caf32b838180e5b392b214d5
Faz <faz.sadeghi@gmail.com>
2017-04-26 18:05:25 +0100

    Update epelteset.pp

--- site/users/manifests/epelteset.pp
+++ site/users/manifests/epelteset.pp
@@ -1,8 +1,8 @@
 
-  1 class users::epelteset {
-  2
-  3  exec { 'yum -y install epel-release' :
-  4         path => '/bin:/usr/bin',
-  5                }
-  6
-  7 }
+   class users::epelteset {
+  
+    exec { 'yum -y install epel-release' :
+           path => '/bin:/usr/bin',
+                  }
+  
+   }
