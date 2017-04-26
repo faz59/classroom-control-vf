6b0ea6741f43e9770d8be8c6968ebe8aaed20373
boozezela <775744d7@opayq.com>
2017-04-26 10:47:30 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/etc',
+    creates => '/etc/motd',
+  }
+  
 }
