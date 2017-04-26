dd45174f46979537d2ebb937073638725b6fa276
gordonchung <Gordon.Chung@ed.ac.uk>
2017-04-26 13:32:31 +0100

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,16 +1,16 @@
 class skeleton {
-file { '/etc/skell':
+file { '/etc/skel':
   ensure  => dir,
   owner   => 'root',
   group   => 'root',
   mode    => '0440',
-  source  => 'puppet:///modules/skell',
+  source  => 'puppet:///modules/skel',
 }
 file { '/etc/skell/.bashrc':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0440',
-  source  => 'puppet:///modules/skell/.bashrc',
+  source  => 'puppet:///modules/skel/.bashrc',
 }
 }
