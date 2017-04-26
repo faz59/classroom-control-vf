9208879e8c9db9489625fd76d592067eb9b241e8
boozezela <775744d7@opayq.com>
2017-04-26 14:34:39 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,12 +6,16 @@ class nginx {
   file { '/var/www':
     ensure => directory,
   }
-  
+
   file {'/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  
+
+  file { '/etc/nginx':
+    ensure => directory,
+  }
+
   file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
