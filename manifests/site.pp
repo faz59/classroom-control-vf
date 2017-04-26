407bc39f3bf57d9fd3ff1126f1b4238765459a6b
Chen Ko <chen.ko@collinsongroup.com>
2017-04-26 10:59:05 +0100

    7.2 fix errors

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
 	#	content => "Hey, We were troubleshooting today again!\n",
 	#}
 	
-	exec { 'cowsay 'Welcome to training.puppetlabs.vm!' > /etc/motd':
+	exec { "cowsay 'Welcome to training.puppetlabs.vm!' > /etc/motd":
 		path => '/usr/bin:/usr/local/bin',
 		creates => '/etc/motd',
 	}
