83cf4f4da93ec0ceec50d2135da152cd051fd412
Elaine Doyle <elainedoyle@users.noreply.github.com>
2017-04-26 11:18:08 +0100

    Adding cowsay 7.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,13 +42,20 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
-  
-  file { '/etc/motd':
-      ensure => file,
-      owner => 'root',
-      group => 'root',
-      mode => '0644',
-      content => "Elaine says Puppet is fun! \n",
-  }
+  # include role::classroom
+  notify { "Hello, my hostname is ${::hostname}" : }
+ 
+ #file { '/etc/motd':
+ #     ensure => file,
+ #     owner => 'root',
+ ##     group => 'root',
+ #     mode => '0644',
+ #     content => "Elaine says Puppet is fun! \n",
+ # }
+
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd" :
+   path => '/usr/bin:/usr/local/bin',
+   creates => '/etc/motd',
+}
+
 }
