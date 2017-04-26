fe422aac85ee6b9e384a2ecdd652bbf56793a324
gordonchung <Gordon.Chung@ed.ac.uk>
2017-04-26 10:50:02 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,13 +44,13 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   #
-  file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Hey, Puppet is fun!\n",
-}
+  #file { '/etc/motd':
+  #ensure  => file,
+  #owner   => 'root',
+  #group   => 'root',
+  #mode    => '0644',
+  #content => "Hey, Puppet is fun!\n",
+#}
 exec { 'cowsay':
   path    => '/usr/local/bin',
   command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
