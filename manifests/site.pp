3bfe2bce93edff663ca126413c198bfb1d11addb
jharihar <jayakumar.hariharan@intercall.com>
2017-04-26 15:18:49 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,5 +42,19 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  file { '/etc/motd':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0644',
+  content => "Today I learned what it means to manage state using Puppet.\n",
+}
+
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+  path => '/usr/bin:/usr/local/bin',
+  creates => '/etc/motd',
+}
+
   include role::classroom
 }
