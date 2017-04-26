7661ac22c2163816ac4cdc7a315330f5aa905f8a
Zed1010 <zed010@gmail.com>
2017-04-26 10:43:22 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,6 +33,20 @@ ini_setting { 'random ordering':
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
 # node definitions.
 
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+notify { "Hello, my name is ${::hostname}": }
+file { '/etc/motd':
+ensure => file,
+owner => 'root',
+group => 'root',
+mode => '0644',
+content => "Today I learned what it means to manage state using Puppet.\n",
+}
+}
+
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
