f5759bde8aa1ed33d248b260201e6b4cd407db57
BlakBlast <omegapl0@wp.pl>
2017-04-26 11:41:56 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,7 +53,7 @@ node default {
  # }
   
   exec { 'change_content':
-    command => '7.2 Welcome to ${::fqdn}! > /etc/motd',
+    command => 'echo "7.2 Welcome to ${::fqdn}!" > /etc/motd',
     path => '/usr/bin',
     onlyif => 'test -f /etc/motd',  
   }   
