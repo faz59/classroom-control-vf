f19eada3442158444d2e2769e2a1d2a391f48e7c
Chen Ko <chen.ko@collinsongroup.com>
2017-04-26 14:04:16 +0100

    Excerise: 10.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,20 +43,4 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
-  
-	#file { '/etc/motd':
-	#	ensure  => file,
-	#	owner   => 'root',
-	#	group   => 'root',
-	#	mode    => '0644',
-	#	content => "Hey, We were troubleshooting today again!\n",
-	#}
-	
-	#exec { "cowsay 'Welcome to training.puppetlabs.vm!' > /etc/motd":
-	#	path => '/usr/bin:/usr/local/bin',
-	#	creates => '/etc/motd',
-	#}
-
-	include user
-	include skeleton
 }
