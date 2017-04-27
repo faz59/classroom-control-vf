564e71cefb844430b327ba1752270e88e5949bca
Jehoszafat Zimnowoda <j.zimnowoda@DIGITAL.local>
2017-04-27 10:59:57 +0200

    Fix

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,30 @@
 class nginx {
 
   package {'nginx'
-    ensure => 'installed'
-
+    ensure => 'installed',
   }
 
   service { 'nginx':
     ensure => running,
     enable => true,
-    require => Package['nginx']
-    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf']
+    require => Package['nginx'],
+    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
   }
 
   file  {'/var/www':
     ensure => 'directory',
-    require => Servie['nginx']
+    require => Servie['nginx'],
   }
 
   file  {'/etc/nginx/nginx.conf':
     ensure => 'file',
     source => 'puppet:///modules/nginx/nginx.conf'
-    require => Servie['nginx']
+    require => Servie['nginx'],
   }
 
   file  {'/etc/nginx/conf.d/default.conf':
     ensure => 'file',
-    source => 'puppet:///modules/nginx/default.conf'
-    require => Servie['nginx']
+    source => 'puppet:///modules/nginx/default.conf',
   }
 
 }
