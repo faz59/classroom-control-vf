236a77c973bef7cee9bee9c096433b404269d8a7
Elaine Doyle <elainedoyle@users.noreply.github.com>
2017-04-27 09:55:51 +0100

    nginx manifest file

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,43 @@
+class nginx {
+
+File { 
+owner => 'root',
+group => 'root',
+mode => '0664',
+}
+
+package { 'nginx':
+ensure => present, 
+}
+
+file { '/var/www':
+ensure => directory
+}
+
+file { '/var/www/index.html' :
+ensure => file, 
+source => 'puppet:///modules/nginx/index.html'
+}  
+
+file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+   notify => Service['nginx'], 
+    
+  }
+
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+     notify => Service['nginx'], 
+  }
+
+service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+  
+} 
