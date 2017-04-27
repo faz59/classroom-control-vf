b9491bd0031d25410fb5ea6db4a405d0c3bc0440
Faz <faz.sadeghi@gmail.com>
2017-04-27 08:18:49 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,15 +6,25 @@ ensure => present,
 
 file { '/var/www':
 ensure => directory
+
+owner => 'root',
+group => 'root',
+mode => '0664',
 }
 
 file { '/var/www/index.html' :
 ensure => file, 
+owner => 'root',
+group => 'root',
+mode => '0664',
 source => 'puppet:///modules/nginx/index.html'
 }  
 
 file { '/etc/nginx/nginx.conf':
     ensure  => file,
+    owner => 'root',
+group => 'root',
+mode => '0664',
     source  => 'puppet:///modules/nginx/nginx.conf',
     
   }
@@ -22,6 +32,9 @@ file { '/etc/nginx/nginx.conf':
 
   file { '/etc/nginx/conf.d/default.conf':
     ensure  => file,
+    owner => 'root',
+group => 'root',
+mode => '0664',
     source  => 'puppet:///modules/nginx/default.conf',
   }
 
